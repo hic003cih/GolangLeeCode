64e1363c4941bc7265abdbe24c96423c3b4548aa
leif <ac676175@gmail.com>
2020-12-28 07:16:15 +0800

    new_1

--- 1_Array/1_TwoSum/README.md
+++ 1_Array/1_TwoSum/README.md
@@ -39,5 +39,9 @@ Only one valid answer exists.
 ```
 
 ## 解題
+1. 暴力法
 用雙層for把nums拆解,第一層用來固定第一個index
 第二層用來執行後面的每個index,然後在最裡面的迴圈加起來,並返回index
+   
+2. 哈希表
+用一個哈希表來將數字存入表中,然後用target-x去尋找這個數值是否等於target的值
--- 1_Array/1_TwoSum/main.go
+++ 1_Array/1_TwoSum/main.go
@@ -1,19 +1,34 @@
 package main
-)
+
+import "fmt"
+
 func main() {
 	nums := []int{3, 2, 4}
 	target := 6
 	x := twoSum(nums, target)
+	fmt.Print(x)
 }
 
 func twoSum(nums []int, target int) []int {
-	for i := 0; i < len(nums); i++ {
-		y := i
-		for y = (y + 1); y < len(nums); y++ {
-			if target == nums[i]+nums[y] {
-				return []int{i, y}
-			}
+	//暴力法
+	//for i := 0; i < len(nums); i++ {
+	//	y := i
+	//	for y = (y + 1); y < len(nums); y++ {
+	//		if target == nums[i]+nums[y] {
+	//			return []int{i, y}
+	//		}
+	//	}
+	//}
+	//return nil
+	//哈希表
+	hashTable := map[int]int{}
+	for i, x := range nums {
+		if p, ok := hashTable[target-x]; ok {
+			fmt.Printf(string(p))
+			return []int{p, i}
 		}
+		hashTable[x] = i
+		fmt.Printf(string(hashTable[x]))
 	}
 	return nil
 }
