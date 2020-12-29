4d74a5221fde42e99311cecc26c92357474ec5f0
LeifChang <leif.chang@liteon.com>
2020-12-29 17:49:40 +0800

    edit_4

--- 1_Array/4_Median_of_Two_Sorted_Arrays/main.go
+++ 1_Array/4_Median_of_Two_Sorted_Arrays/main.go
@@ -1,70 +1,56 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	nums1 := []int{1, 3}
-	nums2 := []int{2, 7}
-	findMedianSortedArrays(nums1, nums2)
+	nums1 := []int{1, 2}
+	nums2 := []int{-1, 3}
+	//findMedianSortedArrays(nums1, nums2)
+	fmt.Println(findMedianSortedArrays(nums1, nums2))
+
 }
 
-func findMedianSortedArrays(mis []int, njs []int) float64 {
+//只能用一次迴圈
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 
-	lenMis, i := len(mis), 0
-	lenNjs, j := len(njs), 0
-	res := make([]int, lenMis+lenNjs)
+	mergeNums := merge(nums1, nums2)
+	fmt.Println(mergeNums)
+	if len(mergeNums) < 2 {
+		fmt.Println(mergeNums)
+		return float64(mergeNums[0])
+	}
+	//如果是奇數
+	if len(mergeNums)%2 == 1 {
+		i := int(len(mergeNums) / 2)
+		fmt.Println(i)
+		return (float64(mergeNums[i]))
+	}
+	i := len(mergeNums) / 2
+	// for {
+	// 	i++
+	// 	j--
+	// 	if i == j || (i+1) == j {
+	// 		break
+	// 	}
+	// }
+	return (float64(mergeNums[i-1]) + float64(mergeNums[i])) / 2.0
 
-	for k := 0; k < lenMis+lenNjs; k++ {
-		//如果i的長度和nums長度一樣
-		//或者i<
-		if i == lenMis ||
-			(i < lenMis && j < lenNjs && mis[i] > njs[j]) {
-			res[k] = njs[j]
-			j++
-			continue
+}
+func merge(nums1 []int, nums2 []int) []int {
+	nums1 = append(nums1, nums2...)
+
+	leftPoint := 0
+	for rightPoint := 1; rightPoint < len(nums1); rightPoint++ {
+		if nums1[leftPoint] > nums1[rightPoint] {
+
+			tempRight := nums1[rightPoint]
+			nums1[rightPoint] = nums1[leftPoint]
+			nums1[leftPoint] = tempRight
 		}
-
-		if j == lenNjs ||
-			(i < lenMis && j < lenNjs && mis[i] <= njs[j]) {
-			res[k] = mis[i]
-			i++
-		}
+		leftPoint++
 	}
 
-	return 0.0
-	// nums1 = append(nums1, nums2...)
-	// sum := 0
-	// newArray := make([]int, len(nums1))
-	// newArray2 := make([]int, len(nums1))
-	// //fmt.Println(newArray)
-	// //fmt.Println(newArray[len(newArray)-1 : len(newArray)])
-	// for i, v := range nums1 {
-
-	// 	/* if i == lenMis ||
-	// 		(i < lenMis && j < lenNjs && mis[i] > njs[j]) {
-	// 		res[k] = njs[j]
-	// 		j++
-	// 		continue
-	// 	} */
-
-	// 	// if i == 0 {
-	// 	// 	newArray = append(newArray, v)
-	// 	// }
-	// 	// for y, v2 := range newArray {
-	// 	// 	fmt.Println(v2)
-	// 	// 	if v > v2 {
-	// 	// 		//fmt.Println(newArray[:y])
-	// 	// 		newArray2 = append(newArray[:y], v)
-	// 	// 		fmt.Println(newArray2)
-	// 	// 	}
-	// 	// }
-	// 	// if v > newArray[len(newArray)-1:len(newArray)] {
-	// 	// 	newArray = append(newArray, v)
-	// 	// }
-	// 	sum += v
-	// }
-	// //fmt.Println(nums1)
-	// //fmt.Println(sum)
-	// //fmt.Println(len(nums1))
-	// //fmt.Println(float64(sum) / float64(len(nums1)))
-	// return float64(sum) / float64(len(nums1))
-
+	return nums1
 }
