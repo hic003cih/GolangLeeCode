9c5c877df517ed6e1dd31a4c097fcba96d5b2af1
LeifChang <leif.chang@liteon.com>
2021-08-18 09:48:24 +0800

    49(T2)

--- Important/String/49_Group Anagrams/main.go
+++ Important/String/49_Group Anagrams/main.go
@@ -13,33 +13,52 @@ func main() {
 	x := groupAnagrams(nums)
 	fmt.Print(x)
 }
-
 func groupAnagrams(strs []string) [][]string {
-
 	mp := map[string][]string{}
 
 	ans := [][]string{}
 
-	for _, str := range strs {
-		//將字串轉為byte
-		s := []byte(str)
-		//使用sort.Slice方法,將array傳入,比大小作排序
+	for _, v := range strs {
+		s := []byte(v)
+
 		sort.Slice(s, func(i, j int) bool {
-			return s[i] < s[j]
+			return s[j] > s[i]
 		})
-		//將排序過的字母byte轉回string
-		sortedStr := string(s)
-		//將排序過一樣的字串,存到同一個索引map
-		//排過的sortedStr當索引,原始的str當值
-		mp[sortedStr] = append(mp[sortedStr], str)
-		fmt.Println(mp[sortedStr])
+		convert := string(s)
+		mp[convert] = append(mp[convert], v)
 	}
-	//循環將map的值存到ans
 	for _, v := range mp {
 		ans = append(ans, v)
 	}
 	return ans
 }
+
+// func groupAnagrams(strs []string) [][]string {
+
+// 	mp := map[string][]string{}
+
+// 	ans := [][]string{}
+
+// 	for _, str := range strs {
+// 		//將字串轉為byte
+// 		s := []byte(str)
+// 		//使用sort.Slice方法,將array傳入,比大小作排序
+// 		sort.Slice(s, func(i, j int) bool {
+// 			return s[i] < s[j]
+// 		})
+// 		//將排序過的字母byte轉回string
+// 		sortedStr := string(s)
+// 		//將排序過一樣的字串,存到同一個索引map
+// 		//排過的sortedStr當索引,原始的str當值
+// 		mp[sortedStr] = append(mp[sortedStr], str)
+// 		fmt.Println(mp[sortedStr])
+// 	}
+// 	//循環將map的值存到ans
+// 	for _, v := range mp {
+// 		ans = append(ans, v)
+// 	}
+// 	return ans
+// }
 
 //正確版
 // func groupAnagrams(strs []string) [][]string {
